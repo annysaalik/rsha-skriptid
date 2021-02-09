ffdc7078df0d81d9be9ec93fe4f3adeae0d43fee
saalikanny <anny-anete.saalik@khk.ee>
2021-02-09 09:40:15 +0200

    mysql-server paigaldusskript on loodud

---
+++ mysql_paigaldamine.sh
@@ -0,0 +1,14 @@
+MYSQL=$(dpkg-qwerty -W -f'${Status}' mysql-server 2>/dev/null | grep -c 'ok installed')
+if [ $MYSQL -eq 0 ]; then
+	echo "Paigaldame mysql ja vajalikud lisad"
+	apt install mysql-server
+	echo "mysql on paigaldatud"
+	touch $HOME/.my.cnf
+	echo "[client]" >> $HOME/.my.cnf
+	echo "host = localhost" >> $HOME/.my.cnf
+	echo "user = root" >> $HOME/.my.cnf
+	echo "password = qwerty" >> $HOME/.my.cnf
+elif [ $MYSQL -eq l ]; then
+	echo "mysql-server on juba paigaldatud"
+	mysql
+fi
